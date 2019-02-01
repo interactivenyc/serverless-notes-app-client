7f0ebcec52a1b8cf76e25ec5f18189d6e4370a49
Steve Warren <steve@interactivenyc.com>
2019-02-01 15:18:54 -0500

    adding loader button

--- src/components/AppliedRoute.js
+++ src/components/AppliedRoute.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React from "react";
+import { Route } from "react-router-dom";
 
 // This class is explained here:
 // https://serverless-stack.com/chapters/add-the-session-to-the-state.html
 
-export default ({ component: C, props: cProps, ...rest }) => (
-    <Route {...rest} render={(props) => <C {...props} {...cProps} />} />
+const AppliedRoute = ({ component: C, props: cProps, ...rest }) => (
+  <Route {...rest} render={props => <C {...props} {...cProps} />} />
 );
+
+export default AppliedRoute;
---
+++ src/components/LoaderButton.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Button, Glyphicon } from "react-bootstrap";
+import "./LoaderButton.css";
+
+const LoaderButton = ({
+  isLoading,
+  text,
+  loadingText,
+  className = "",
+  disabled = false,
+  ...props
+}) => (
+  <Button
+    className={`LoaderButton ${className}`}
+    disabled={disabled || isLoading}
+    {...props}
+  >
+    {isLoading && <Glyphicon glyph="refresh" className="spinning" />}
+    {!isLoading ? text : loadingText}
+  </Button>
+);
+
+export default LoaderButton;
