a555e253e48d115c5fa6e9a458065f9a0126d977
Steve Warren <steve@interactivenyc.com>
2019-02-04 13:58:02 -0500

    signup and new note

--- src/Routes.js
+++ src/Routes.js
@@ -3,13 +3,24 @@ import { Route, Switch } from "react-router-dom";
 import AppliedRoute from "./components/AppliedRoute";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
+import Signup from "./containers/Signup";
+import NewNote from "./containers/NewNote";
 import NotFound from "./containers/NotFound";
 
-export default ({ childProps }) => (
+const Routes = ({ childProps }) => (
   <Switch>
     <AppliedRoute path="/" exact component={Home} props={childProps} />
     <AppliedRoute path="/login" exact component={Login} props={childProps} />
+    <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
+    <AppliedRoute
+      path="/notes/new"
+      exact
+      component={NewNote}
+      props={childProps}
+    />
     {/* Finally, catch all unmatched routes */}
     <Route component={NotFound} />
   </Switch>
 );
+
+export default Routes;
